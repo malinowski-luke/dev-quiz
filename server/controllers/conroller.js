6bca14fb011de0d076d5353fe7cb0fe4c96a3596
Lukasz Malinowski <lukaszmalinowski@MacBook-Pro.local>
2020-09-07 19:05:25 -0400

    quizes done

--- server/controllers/conroller.js
+++ server/controllers/conroller.js
@@ -1,12 +1,15 @@
 const landingData = require('../data/landingData')
 const quizzes = require('../data/quizQuestions')
 
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5)
+
 module.exports = {
   getLandingData: (req, res) => {
     return res.status(200).send(landingData)
   },
   getQuiz: (req, res) => {
     const { subject } = req.params
-    return res.status(200).send(quizzes[subject])
+    const quiz = shuffle([...quizzes[subject]])
+    return res.status(200).send(quiz)
   },
 }
--- server/data/quizQuestions.js
+++ server/data/quizQuestions.js
@@ -13,13 +13,274 @@ module.exports = {
     },
     {
       id: 2,
-      question: 'The # symbol specifies that the selector is?',
+      question: 'How do you write an image tag in HTML5?',
+      correctAnswer: 'd',
+      answers: {
+        a: '<img src="" ></img>',
+        b: '<img src="">',
+        c: '<img src=""><img/>',
+        d: '<img src=""/>',
+      },
+    },
+    {
+      id: 3,
+      question: `What is the correct JavaScript syntax to change the content of the HTML element below?\n<p id="demo">Hello</p>`,
       correctAnswer: 'b',
       answers: {
-        a: 'class',
-        b: 'id',
-        c: 'tag',
-        d: 'first',
+        a: 'document.getElement(“demo”).innerHTML=”Hello World”',
+        b: 'document.getElementById(“demo”).innerHTML=”Hello World”',
+        c: 'document.getId("demo")=”Hello World"',
+        d: 'document.getElementById("demo").innerHTML=Hello World',
+      },
+    },
+    {
+      id: 4,
+      question: 'What are the two methods of forms transfer?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'Get and Receive',
+        b: 'Post and Receive',
+        c: 'Get and Post',
+        d: 'Post and Take',
+      },
+    },
+    {
+      id: 5,
+      question: 'What should be the very first thing in an HTML document?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Doc Type',
+        b: 'Body',
+        c: 'Title',
+        d: 'Heading',
+      },
+    },
+    {
+      id: 6,
+      question: 'Inside which HTML element do we put the JavaScript?',
+      correctAnswer: 'b',
+      answers: {
+        a: '<js>',
+        b: '<script>',
+        c: '<scripting>',
+        d: '<javascript>',
+      },
+    },
+    {
+      id: 7,
+      question:
+        'Which of the following is the correct syntax to display “Dev Quiz” in an alert box using JavaScript?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'alertBox("Dev Quiz")',
+        b: 'msg(“Dev Quiz”)',
+        c: 'alert("Dev Quiz")',
+        d: 'msgbox(“Dev Quiz”)',
+      },
+    },
+    {
+      id: 8,
+      question:
+        'Which of the following Css selectors matches all elements of a type?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Type',
+        b: 'Universal',
+        c: 'Descendant',
+        d: 'Class',
+      },
+    },
+    {
+      id: 9,
+      question: 'How can you created rounded corners using CSS?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'border[round]: 30px;',
+        b: 'corner-effect: round;',
+        c: 'border-radius: 30px;',
+        d: 'alpha-effect: round-corner;',
+      },
+    },
+    {
+      id: 10,
+      question: 'How to rotate objects using CSS?',
+      correctAnswer: 'd',
+      answers: {
+        a: 'rotate-object: 30deg;',
+        b: 'object-rotation: 30deg;',
+        c: 'object-rotation: 30deg;',
+        d: 'transform: rotate(30deg);',
+      },
+    },
+    {
+      id: 11,
+      question: 'How many tags are in a regular HTML element??',
+      correctAnswer: '2',
+      answers: {
+        a: '1',
+        b: '2',
+        c: '3',
+      },
+    },
+    {
+      id: 12,
+      question:
+        ' In JavaScript, we do not have datatypes like integer and float. What is the function that can be used to check if the number is an integer or not?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'Integer(value)',
+        b: 'ifInteger(value)',
+        c: 'isInteger(value)',
+        d: 'ifinteger(value)',
+      },
+    },
+    {
+      id: 13,
+      question:
+        'What is the syntax for writing a single line comment in javascript',
+      correctAnswer: 'b',
+      answers: {
+        a: '<!–This is a comment–&gt',
+        b: '//This is a comment',
+        c: '–This is a comment',
+        d: '**This is a comment**',
+      },
+    },
+    {
+      id: 14,
+      question:
+        'What is the method in JavaScript used to remove the whitespace at the beginning and end of any string?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'trimmed()',
+        b: 'stripped()',
+        c: 'trim()',
+        d: 'strip()',
+      },
+    },
+    {
+      id: 15,
+      question:
+        'Which of the following HTML Elements is used for making any text bold?',
+      correctAnswer: 'b',
+      answers: {
+        a: '<p>',
+        b: '<b>',
+        c: '<li>',
+        d: '<i>',
+      },
+    },
+    {
+      id: 16,
+      question:
+        'Which of the following is the correct way of creating an hyperlink in HTML?',
+      correctAnswer: 'd',
+      answers: {
+        a: '<a>www.dev-quiz.org <dev-quiz /a>',
+        b: '<a href=“www.dev-quiz.org” dev-quiz /a>',
+        c: '<a link=“www.dev-quiz.org” dev-quiz> </a>',
+        d: '<a href= “www.dev-quiz.org”>dev-quiz</a>',
+      },
+    },
+    {
+      id: 17,
+      question: 'What is the purpose of using div tags in HTML?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'For creating different styles.',
+        b: 'For adding headings.',
+        c: 'For creating different sections.',
+        d: 'For adding titles.',
+      },
+    },
+    {
+      id: 18,
+      question:
+        'Which of the following is the correct way to add background color in HTML?',
+      correctAnswer: 'a',
+      answers: {
+        a: '<body style = “background-color: green;”>',
+        b: '<body bg-color = “green”>',
+        c: '<body style = “background-color=green”>',
+        d: '<body color = “green”>',
+      },
+    },
+    {
+      id: 19,
+      question:
+        'Which of the following elements can be used in HTML to create a table?',
+      correctAnswer: 'a',
+      answers: {
+        a: '<table> , <tbody> , <tr>',
+        b: '<table> , <tbody> , <trow>',
+        c: '<table> , <tb> , <trow>',
+        d: 'All of the above',
+      },
+    },
+    {
+      id: 20,
+      question: 'Which tag is used for creating a drop-down selection list?',
+      correctAnswer: 'd',
+      answers: {
+        a: '<option>',
+        b: '<list>',
+        c: '<dropdown>',
+        d: '<select>',
+      },
+    },
+    {
+      id: 21,
+      question: 'Does the box-shadow support all browsers?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'true',
+        b: 'false',
+      },
+    },
+    {
+      id: 22,
+      question:
+        'Which prefix do you need to get CSS3 properties to work on older Mozilla Firefox browsers?',
+      correctAnswer: 'a',
+      answers: {
+        a: '-webkit',
+        b: '-o-',
+        c: '-gecko-',
+        d: '-moz',
+      },
+    },
+    {
+      id: 23,
+      question:
+        'Which prefix do you need to get CSS3 properties to work on older Mozilla Firefox browsers?',
+      correctAnswer: 'a',
+      answers: {
+        a: '-webkit',
+        b: '-o-',
+        c: '-gecko-',
+        d: '-moz',
+      },
+    },
+    {
+      id: 24,
+      question: 'How to create transition effects using CSS?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'word-wrap: break-word;',
+        b: 'words-wrap: break-word;',
+        c: 'text-wrap: break-word;',
+        d: 'text-width: word-wrap;',
+      },
+    },
+    {
+      id: 25,
+      question: 'How to resize a background image using CSS?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'background-proportion: 80px 60px;',
+        b: 'alpha-effect: bg-resize 80px 60px;',
+        c: 'background-size: 80px 60px;',
+        d: 'None',
       },
     },
   ],
@@ -37,14 +298,263 @@ module.exports = {
     },
     {
       id: 2,
-      question: 'The # symbol specifies that the selector is?',
+      question: 'All APIs of Node.JS are?',
       correctAnswer: 'b',
-
       answers: {
-        a: 'class',
-        b: 'id',
-        c: 'tag',
-        d: 'first',
+        a: 'Synchronous',
+        b: 'Asynchronous',
+        c: 'All of The Above',
+        d: 'None of The Above',
+      },
+    },
+    {
+      id: 3,
+      question: 'JavaScript ignores extra spaces.',
+      correctAnswer: 'b',
+      answers: {
+        a: 'True',
+        b: 'False',
+      },
+    },
+    {
+      id: 4,
+      question: 'File is a server-side JavaScript object?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'True',
+        b: 'False',
+      },
+    },
+    {
+      id: 5,
+      question:
+        'Which of the following method is used to evaluate a string of Java Script code in the context of the specified object?',
+      correctAnswer: 'd',
+      answers: {
+        a: 'parseDouble',
+        b: 'eFloat',
+        c: 'parseObject',
+        d: 'eval',
+      },
+    },
+    {
+      id: 6,
+      question: 'How to append a value to an array in JavaScript?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'arr[arr.length+1] = new Arrays()',
+        b: 'arr[arr.length-1] = value',
+        c: 'arr[arr.length] = value',
+        d: 'arr[arr.length*1] = value',
+      },
+    },
+    {
+      id: 7,
+      question:
+        '_____ is a JavaScript keyword that creates, or declares, a new variable.',
+      correctAnswer: 'c',
+      answers: {
+        a: 'bar',
+        b: 'console',
+        c: 'var',
+        d: 'def',
+      },
+    },
+    {
+      id: 8,
+      question:
+        'The "const" keyword signals that the variable cannot be changed',
+      correctAnswer: 'a',
+      answers: {
+        a: 'True',
+        b: 'False',
+      },
+    },
+    {
+      id: 9,
+      question:
+        'Select a String function that finds the match between a regular expression and a string, and to replace the matched substring with a new substring?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'concat()',
+        b: 'replace()',
+        c: 'search()',
+        d: 'match()',
+      },
+    },
+    {
+      id: 10,
+      question: 'How do you find the minimum of x and y using JavaScript?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Math.min(x,y)',
+        b: 'min(xy)',
+        c: 'Math.min(xy)',
+        d: 'minimum(x,y)',
+      },
+    },
+    {
+      id: 11,
+      question:
+        'Which method will you use to round the number 24.76 to the nearest integer?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'round(24.76)',
+        b: 'Math.round(24.76)',
+        c: 'rnd(24.76)',
+        d: 'Math.rnd(24.76)',
+      },
+    },
+    {
+      id: 12,
+      question: 'Which of the following is an event listener in JavaScript?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'onclick',
+        b: 'click',
+        c: 'Click()',
+        d: 'onchange',
+      },
+    },
+    {
+      id: 13,
+      question:
+        'You are a web designer in your company and need to create a JavaScript project for form validation. What are the advantages that JavaScript provides for forms?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'Bandwidth Conservation',
+        b: 'Form Validation',
+        c: 'All of The Above',
+      },
+    },
+    {
+      id: 14,
+      question: 'Which of the given options is an incorrect variable name?',
+      correctAnswer: 'd',
+      answers: {
+        a: 'foo',
+        b: '_foo',
+        c: '$foo',
+        d: '-foo',
+      },
+    },
+    {
+      id: 15,
+      question: 'What is the syntax of a "for" statement in JavaScript?',
+      correctAnswer: 'd',
+      answers: {
+        a: 'for(increment; condition; initialization)',
+        b: 'for(initialization, condition, increment)',
+        c: 'for(condition; initialization; increment)',
+        d: 'for(initialization; condition; increment)',
+      },
+    },
+    {
+      id: 16,
+      question:
+        'If we declare a variable, let test = 1, then later, reassign, stating test = 2, what will happen?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Test Will Equal 2',
+        b: 'Test Will Equal 1',
+        c: 'TypeError',
+        d: 'Test Will Equal Undefined',
+      },
+    },
+    {
+      id: 17,
+      question:
+        'he != and !== symbols both work the same for inequality comparisons?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'True',
+        b: 'False',
+      },
+    },
+    {
+      id: 18,
+      question: 'For strict equality comparisons, we should use?',
+      correctAnswer: 'c',
+      answers: {
+        a: '=',
+        b: '==',
+        c: '===',
+      },
+    },
+    {
+      id: 19,
+      question: 'Function is Used To Parse a String To Int?',
+      correctAnswer: 'b',
+      answers: {
+        a: 'Integer.Parse',
+        b: 'Int.Parse',
+        c: 'Parse.Int',
+        d: 'None',
+      },
+    },
+    {
+      id: 20,
+      question: 'IsNaN() Evaluates And Argument To Determine if Given Value',
+      correctAnswer: 'b',
+      answers: {
+        a: 'Is Not a Null',
+        b: 'Is Not a Number',
+        c: 'Is Not a New Object',
+        d: 'None of the above',
+      },
+    },
+    {
+      id: 21,
+      question: 'A Function Associated With An object is Called?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'Constructor',
+        b: 'Function',
+        c: 'Method',
+        d: 'None of the above',
+      },
+    },
+    {
+      id: 22,
+      question: 'Date util function GetMonth() returns The Month as',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Int',
+        b: 'Float',
+        c: 'Char',
+        d: 'String',
+      },
+    },
+    {
+      id: 23,
+      question: 'In Node a process is?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Global Object',
+        b: 'Local Object',
+      },
+    },
+    {
+      id: 24,
+      question:
+        'In a switch....case....default construct where do we put the expression we are going to evaluate?',
+      correctAnswer: 'a',
+      answers: {
+        a: 'Case',
+        b: 'Default',
+        c: 'Switch',
+        d: 'None',
+      },
+    },
+    {
+      id: 25,
+      question: 'Which construct do we use with errors?',
+      correctAnswer: 'c',
+      answers: {
+        a: 'do...while...',
+        b: 'if...else...',
+        c: 'try...catch...finally...',
+        d: 'switch...case...default...',
       },
     },
   ],
